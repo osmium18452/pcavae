376d8dad574ea68d3f8a9c7e0ec016d336180c32
17110 <1711042219@qq.com>
2023-02-13 16:49:33 +0800

    serial training dome

--- IVAE.py
+++ IVAE.py
@@ -26,7 +26,7 @@ class IVAE:
         label_set = dataloader.load_label_set()
 
         self.vae_dim_list = dataloader.load_vae_dim_list()
-        print('vae dim list:',self.vae_dim_list)
+        print('vae dim list:', self.vae_dim_list)
 
         self.cnn_train_set_x = torch.Tensor(cnn_train_set_x)
         self.cnn_train_set_y = torch.Tensor(cnn_train_set_y)
@@ -53,26 +53,51 @@ class IVAE:
                 self.vae_list[i].cuda(device=self.device_list[i])
             self.optimizer_list.append(optim.Adam(self.vae_list[i].parameters(), lr=learning_rate))
 
-    def train_single_vae_one_epoch(self, vae_no, batch_size, gpu):
+    def train_single_vae_one_epoch(self, vae_no, batch_size, gpu=False, pbar=False) -> np.ndarray:
         num_iter = self.train_set_size // batch_size
-        for i in range(num_iter):
-            batch_x = self.vae_train_set[vae_no][i * batch_size:(i + 1) * batch_size]
-            if gpu:
-                batch_x = batch_x.cuda(self.device_list[vae_no])
-            recon, mu, log_std = self.vae_list[vae_no](batch_x)
-            self.optimizer_list[vae_no].zero_grad()
-            loss = self.vae_list[vae_no].loss_function(recon, batch_x, mu, log_std)
-            loss.backward()
-            self.optimizer_list[vae_no].step()
+        if pbar:
+            for i in range(num_iter):
+                batch_x = self.vae_train_set[vae_no][i * batch_size:(i + 1) * batch_size]
+                if gpu:
+                    batch_x = batch_x.cuda(self.device_list[vae_no])
+                recon, mu, log_std = self.vae_list[vae_no](batch_x)
+                self.optimizer_list[vae_no].zero_grad()
+                loss = self.vae_list[vae_no].loss_function(recon, batch_x, mu, log_std)
+                loss.backward()
+                self.optimizer_list[vae_no].step()
+                mse=torch.mean((recon-batch_x)**2).item()
+                self.subpbar.set_postfix_str("mse loss: %.5e" % (mse))
+                self.subpbar.update()
 
-        if num_iter * batch_size < self.train_set_size:
-            batch_x = self.vae_train_set[vae_no][num_iter * batch_size:]
-            if gpu:
-                batch_x = batch_x.cuda(self.device_list[vae_no])
-            recon, mu, log_std = self.vae_list[vae_no](batch_x)
-            self.optimizer_list[vae_no].zero_grad()
-            loss = self.vae_list[vae_no].loss_function(recon, batch_x, mu, log_std)
-            self.optimizer_list[vae_no].step()
+            if num_iter * batch_size < self.train_set_size:
+                batch_x = self.vae_train_set[vae_no][num_iter * batch_size:]
+                if gpu:
+                    batch_x = batch_x.cuda(self.device_list[vae_no])
+                recon, mu, log_std = self.vae_list[vae_no](batch_x)
+                self.optimizer_list[vae_no].zero_grad()
+                loss = self.vae_list[vae_no].loss_function(recon, batch_x, mu, log_std)
+                loss.backward()
+                self.optimizer_list[vae_no].step()
+        else:
+            for i in range(num_iter):
+                batch_x = self.vae_train_set[vae_no][i * batch_size:(i + 1) * batch_size]
+                if gpu:
+                    batch_x = batch_x.cuda(self.device_list[vae_no])
+                recon, mu, log_std = self.vae_list[vae_no](batch_x)
+                self.optimizer_list[vae_no].zero_grad()
+                loss = self.vae_list[vae_no].loss_function(recon, batch_x, mu, log_std)
+                loss.backward()
+                self.optimizer_list[vae_no].step()
+
+            if num_iter * batch_size < self.train_set_size:
+                batch_x = self.vae_train_set[vae_no][num_iter * batch_size:]
+                if gpu:
+                    batch_x = batch_x.cuda(self.device_list[vae_no])
+                recon, mu, log_std = self.vae_list[vae_no](batch_x)
+                self.optimizer_list[vae_no].zero_grad()
+                loss = self.vae_list[vae_no].loss_function(recon, batch_x, mu, log_std)
+                loss.backward()
+                self.optimizer_list[vae_no].step()
 
         if gpu:
             validate_set = self.vae_validate_set[vae_no].cuda(self.device_list[vae_no])
@@ -81,24 +106,61 @@ class IVAE:
         recon, mu, log_std = self.vae_list[vae_no](validate_set)
         # print(recon.shape)
         # print('recon:',recon[1].cpu().detach().numpy(),'\nval:',validate_set[1].cpu().numpy())
-        return torch.squeeze(recon[:,0])
+        return torch.squeeze(recon[:, 0]).cpu().detach().numpy()
 
     # test train
-    def train_simultaneously(self, epoch, batch_size, gpu):
-        pbar=tqdm(total=epoch, ascii=True)
+    def train_in_parallel(self, epoch, batch_size, gpu, proc=None):
+        pbar = tqdm(total=epoch, ascii=True)
         pbar.set_postfix_str("mse loss: -.-----e---")
+        # build validate set, useful data only
+        val_list = []
+        for i in range(self.vae_num):
+            val_list.append(np.squeeze(self.vae_validate_set[i][:, 0].numpy()))
+        val_list = np.array(val_list)
+        print(val_list.shape)
         for i in range(epoch):
             # shuffle data every 5 epochs
             if i % 5 == 0:
                 shuffle = np.random.permutation(self.train_set_size)
                 for i in range(self.vae_num):
                     self.vae_train_set[i] = self.vae_train_set[i][shuffle]
-            recon = self.train_single_vae_one_epoch(0, batch_size, gpu)
-            # print(recon.shape)
-            mse=torch.mean((recon.cpu()-torch.squeeze(self.vae_validate_set[0][:,0]))**2)
+            pool = mp.Pool(proc)
+            arg_list = []
+            for i in range(self.vae_num):
+                arg_list.append((i, batch_size, gpu))
+            recon_list = np.array(pool.map(self.train_single_vae_one_epoch, arg_list))
+            print(recon_list.shape)
+            mse = 1.
+            # train single vae
+            recon_list = np.array(recon_list)
+            mse = np.mean((recon_list - val_list) ** 2)
             pbar.set_postfix_str("mse loss: %.5e" % (mse))
             pbar.update()
         pbar.close()
+
+    def train_in_serial(self, epoch, batch_size, gpu):
+        with tqdm(total=epoch, ascii=True) as pbar:
+            pbar.set_postfix_str("mse loss: -.-----e---")
+            val_list = []
+            for i in range(self.vae_num):
+                val_list.append(np.squeeze(self.vae_validate_set[i][:, 0].numpy()))
+            val_list = np.array(val_list)
+            # print('val list shape', val_list.shape)
+            for i in range(epoch):
+                # shuffle data every 5 epochs
+                if i % 5 == 0:
+                    shuffle = np.random.permutation(self.train_set_size)
+                    for i in range(self.vae_num):
+                        self.vae_train_set[i] = self.vae_train_set[i][shuffle]
+                recon_list = []
+                num_iter = self.train_set_size // batch_size
+                with tqdm(total=num_iter * self.vae_num, ascii=True, leave=False) as self.subpbar:
+                    for i in range(self.vae_num):
+                        recon_list.append(self.train_single_vae_one_epoch(i, batch_size, gpu, True))
+                recon_list = np.array(recon_list)
+                mse = np.mean((recon_list - val_list) ** 2)
+                pbar.set_postfix_str("mse loss: %.5e" % (mse))
+                pbar.update()
 
     def get_device_list(self, gpu_devices, vae_num):
         ret = []
--- main.py
+++ main.py
@@ -38,11 +38,13 @@ if __name__ == '__main__':
     parser.add_argument('-l', "--latent", default=5, type=int)
     parser.add_argument('-G', "--gpu", action="store_true")
     parser.add_argument("-r", "--learning_rate", default=0.01, type=float)
-    parser.add_argument("-e", "--epoch", default=1000, type=int)
+    parser.add_argument("-e", "--epoch", default=100, type=int)
     parser.add_argument("-b", "--batch_size", default=1024, type=int)
     parser.add_argument("-w", "--window_size", default=20, type=int)
     parser.add_argument("-g", "--gpu_device", default="0", type=str)
     parser.add_argument('-N', '--normalize_data', action='store_true')
+    parser.add_argument('-p','--process',default=None,type=int)
+    parser.add_argument('-P','--parallel',action='store_true')
     args = parser.parse_args()
 
     latent_size = args.latent
@@ -53,6 +55,8 @@ if __name__ == '__main__':
     window_size = args.window_size
     gpu_device = args.gpu_device
     normalize = args.normalize_data
+    process=args.process
+    parallel=args.parallel
 
     data_dir = '/remote-home/liuwenbo/pycproj/tsdata/data'
     dataset = 'smd'
@@ -65,4 +69,7 @@ if __name__ == '__main__':
 
     ivae = IVAE(train_set_file, test_set_file, label_file, map_file, latent_size, gpu, learning_rate, window_size,
                 gpu_device, normalize)
-    ivae.train_simultaneously(epoch,batch_size,gpu)
+    if parallel:
+        ivae.train_in_parallel(epoch, batch_size, gpu, proc=process)
+    else:
+        ivae.train_in_serial(epoch, batch_size, gpu)
